e0ebdd3f1c100ed93b78dc4bcd5932c7ab46af4e
Liza <travelling.circus@yandex.ru>
2023-11-04 22:52:04 +0300

    доработка задачи

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -22,32 +22,3 @@ function checkHole(n) {
 function getHole(n) {
     return document.getElementById(`hole${n}`);
 }
-// getHole = (index) => document.getElementById(`hole${index}`);
-//    let a = getHole(1);
-
-// console.log(a);
-
-
-// (() => {
-//     let playing = true,
-//       activeHole = 1;
-  
-//     const stop = () => playing = true,
-//       getHole = index => document.getElementById(`hole${index}`),
-//       deactivateHole = index =>
-//         getHole( index ).className = 'hole',
-//       activateHole = index =>
-//         getHole( index ).className = 'hole hole_has-mole',
-//       next = () => setTimeout(() => {
-//         if ( !playing ) {
-//           return;
-//         }
-//         deactivateHole( activeHole );
-//         activeHole = Math.floor( 1 + Math.random() * 9 );
-//         activateHole( activeHole );
-//         next();
-//       }, 800 );
-  
-//     next();
-//   })();
-  
