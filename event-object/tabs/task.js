21588ab0ea5159375ee70d80d90367a53f3dbae5
Liza <travelling.circus@yandex.ru>
2024-01-24 16:47:22 +0300

    Правки по цвету шрифта вкладок

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -6,10 +6,14 @@ for (i = 0; i < tabsList.length; i++) {
 
 function tabActivate(event) {
   const a = tabsList.indexOf(event.target);
-  const b = contentList.filter((elem) => contentList.indexOf(elem) !== a)
+  const b = contentList.filter((elem) => contentList.indexOf(elem) !== a);
+  const c = tabsList.filter((elem) => tabsList.indexOf(elem) !== a)
   event.target.classList.add("tab_active");
   contentList[a].classList.add("tab__content_active");
   for (i = 0; i < b.length; i++) {
     b[i].classList.remove("tab__content_active"); 
   }
-}
+  for (i = 0; i < c.length; i++) {
+    c[i].classList.remove("tab_active");
+  }
+  }
