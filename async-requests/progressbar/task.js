dcf28f36ed4355d1882d1316f4e55c1b8c6b5795
Liza <travelling.circus@yandex.ru>
2024-02-18 19:39:24 +0300

    Доработано решение задачи progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,12 +1,23 @@
 const loadRequest = new XMLHttpRequest();
-const fileInput = document.getElementById("file");
-const formData = new FormData();
-formData.append('file', fileInput.files[0]);
+const form = document.getElementsByTagName("form");
+const progress = document.getElementById("progress");
+form[0].addEventListener("submit", submitFile);
 
+function submitFile(event) {
+    event.preventDefault(); 
+    const formData = new FormData();
+    const fileInput = document.getElementById("file");
+    formData.append('file', fileInput.files[0]);
+    loadRequest.upload.onprogress = function(event) {
+        a = Number(event.loaded) / Number(event.total / 1000);
+        let intervalId = setInterval(() => progress.value = a, 50);
+    } 
 
-loadRequest.upload.onprogress = function(event) {
-        console.log(event.loaded + ' / ' + event.total);
+    loadRequest.open("POST", "https://students.netoservices.ru/nestjs-backend/upload", true);
+    loadRequest.send(formData);
+   
 }
 
-loadRequest.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
-loadRequest.send(formData);
+
+
+
